e7d1c7b07d41c8f85c83f5dc6e0438ea03215a55
Graciela Alaniz <alanizcreative@gmail.com>
2026-08-14 09:40:18 -0400

    fix: allow hero in blank template

--- src/components/Layout/Layout.ts
+++ src/components/Layout/Layout.ts
@@ -83,6 +83,9 @@ const Layout = (args: LayoutArgs): string => {
   if (!isBlank) {
     headerOutput = Header(slug, baseType)
     footerOutput = Footer(slug, baseType)
+  }
+
+  if (slug !== 'protect') {
     heroOutput = Hero({
       ...itemData,
       ...hero
